648356838c1240f0c10b304263f8e40f87e95656
ASLAN USSAYEV <usprof777@gmail.com>
2015-08-09 23:03:30 -0500

    Add a proto-feed to the Home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,11 @@ class User < ActiveRecord::Base
       update_attribute(:remember_digest, nil)
     end
 
+    # Defines a proto-feed.
+    # See "Following users"  for the full implementation.
+
+    def feed
+      Micropost.where("user_id = ?", id)
+    end
+
 end
